c12b3c90db7a76aa8f91c5b1b9857f293d8f0819
Greg Jones <gjones@banjo.sys.cloudquadrant.com>
2017-05-18 22:10:58 -0700

    added vmware_sizing_from_dialog.rb

---
+++ vmware_sizing_from_dialog.rb
@@ -0,0 +1,23 @@
+task = $evm.root['service_template_provision_task']
+vm_size = $evm.root['dialog_vm_size']
+
+task.miq_request_tasks.each do |child_task|
+  case vm_size
+  when "small"
+    num_sockets = 1
+    num_cores = 1
+    mem_size = '2048'
+  when "medium"
+    num_sockets = 2
+    num_cores = 1
+    mem_size = '4096'
+  when "large"
+    num_sockets = 2
+    num_cores = 2
+    mem_size = '8192'
+  end
+
+  child_task.set_option(:number_of_sockets, num_sockets)
+  child_task.set_option(:cores_per_socket, num_cores)
+  child_task.set_option(:vm_memory, mem_size)
+end
